0b73d5e4904a5f6cb81be4a4142e67e9a827f37c
Alesha Ray <alesha.ray@g.austincc.edu>
2022-10-28 19:49:12 -0500

    update docker build file

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+docker-compose up -d
 docker build -t quizzes --build-arg choice=$1 .
 echo
-docker run --privileged -it --rm -v $(pwd):/app quizzes
+docker run --privileged -it --rm --network=mcq_mcq -v $(pwd):/app quizzes
+# docker-compose up --no-log-prefix --build quizzes -d mongodb mongo-seed

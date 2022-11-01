a2bd2f0857c481599e2adefaad4c35de01932605
Alesha Ray <alesha.ray@g.austincc.edu>
2022-11-01 15:21:44 -0500

    remove unneeded azure project

--- azure_fundamentals_mcq/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t quiz .
-echo
-echo "generating quiz..."
-docker run --privileged -it --rm -v $(pwd):/app quiz
--- azure_fundamentals_mcq/src/main/java/com/aleshamaray/azure_fundamentals_mcq/Constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.aleshamaray.azure_fundamentals_mcq.Constants;
-
-public class Constants {
-  public static final String MULTI_ANSWER = "Multiple Answer";
-  public static final String MULTI_CHOICE = "Multiple Choice";
-  public static final String TRUE_FALSE = "Multiple Choice";
-  public static final String FILL_BLANK = "Fill in the blank";
-}
